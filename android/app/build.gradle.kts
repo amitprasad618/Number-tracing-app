086397fde0051f9084965b4dd47af09aa6877bec
amitprasad618 <amitprasad618@gmail.com>
2026-09-05 19:01:35 +0530

    first admob ads.txt added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,10 +4,39 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
+
     namespace = "com.amitprasad.kidsnumbertracing"
+
     compileSdk = flutter.compileSdkVersion
+
     ndkVersion = flutter.ndkVersion
+
+    signingConfigs {
+
+        create("release") {
+
+            keyAlias = keystoreProperties["keyAlias"] as String
+
+            keyPassword = keystoreProperties["keyPassword"] as String
+
+            storeFile = file(keystoreProperties["storeFile"] as String)
+
+            storePassword = keystoreProperties["storePassword"] as String
+
+        }
+
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -15,23 +44,32 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+
         applicationId = "com.amitprasad.kidsnumbertracing"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
         minSdk = flutter.minSdkVersion
+
         targetSdk = flutter.targetSdkVersion
+
         versionCode = flutter.versionCode
+
         versionName = flutter.versionName
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+
+        getByName("release") {
+
+            signingConfig = signingConfigs.getByName("release")
+
+            isMinifyEnabled = false
+
+            isShrinkResources = false
+
         }
+
     }
+
 }
 
 kotlin {
